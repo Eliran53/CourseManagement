2dcc1cdaab28fed00a27cd71c606f82ba15f3b7c
eliran <elirantarka7@gmail.com>
2020-09-24 16:29:11 +0300

    remove id from schema

--- modules/instructor-module.js
+++ modules/instructor-module.js
@@ -3,7 +3,6 @@ const schema = mongoose.Schema
 
 const instructor = new schema(
     {
-        _id:{type:String,required:true},
         first_name:{type:String,required:true},
         last_name:{type:String,required:true},
         email:{type:String,required:true},
