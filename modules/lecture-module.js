1b0818d0bb92445749451b14eabf143390e74ff5
revitalmatoko <revitalmatoko@gmail.com>
2020-09-25 04:53:29 +0300

    please review

---
+++ modules/lecture-module.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema
+const lecture = new schema(
+    {
+    instructorID:{type:String,required:true},
+    lectureName:{type:String,required:true},
+    subjectID :{type:String,required:true, unique:true},
+    duration:{type:String,required:true},
+    maxCapacityParticipants:{type:Number,required:true},
+    uploadDate:{type:Date,required:true},
+    lectureDate:{type:Date,required:true},
+    summery:{type:String,required:true},
+    price:{type:Number,required:true},
+    language:{type:String,required:true},
+    rank:{type:String},
+    recommended:{type:String,required:true},
+    videos:{type:String,required:true},
+    customerID:{type:String}
+     },
+    {timestamps:true},
+    
+)
+module.exports = mongoose.model("lecture", lecture)
