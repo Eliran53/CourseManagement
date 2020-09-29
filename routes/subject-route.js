ad09ff5cc2bf58605e00841bdefc55bdedf2cf8f
eliran <elirantarka7@gmail.com>
2020-09-29 14:06:17 +0300

    update name of function

--- routes/subject-route.js
+++ routes/subject-route.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const SubjectCtrl = require('../controllers/role-ctrl');
+const SubjectCtrl = require('../controllers/subject-ctrl');
 
 const router = express.Router();
 
-router.post('/',SubjectCtrl.createRole)
-router.get('/',SubjectCtrl.getAllRoles)
-router.put('/:id',SubjectCtrl.updateRole)
-router.delete('/:id',SubjectCtrl.deleteRole)
-router.get('/:id',SubjectCtrl.getRoleById)
+router.post('/',SubjectCtrl.createSubject)
+router.get('/',SubjectCtrl.getAllSubjects)
+router.put('/:id',SubjectCtrl.updateSubject)
+router.delete('/:id',SubjectCtrl.deleteSubject)
+router.get('/:id',SubjectCtrl.getSubjectNameById)
 module.exports = router;
