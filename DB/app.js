9b1e21574f1adc9863b95606658e191c1aa15512
eliran <elirantarka7@gmail.com>
2020-09-29 14:03:55 +0300

    add mongoose set

--- DB/app.js
+++ DB/app.js
@@ -2,6 +2,7 @@ var mongoose= require('mongoose')
 
 const url = "mongodb+srv://lecture867:Aa123456!@cluster0.f5jos.mongodb.net/FinalProject?retryWrites=true&w=majority";
 mongoose.set('useUnifiedTopology', true);
+mongoose.set('useCreateIndex', true)
 mongoose
 .connect(url,{useNewUrlParser:true})
 .catch(e=>{console.error('connection error',e.message)});
