8e73ba66b8eaa3fac2d5973d8d74cf91fd8d9cc8
eliran <elirantarka7@gmail.com>
2020-10-08 00:43:59 +0300

    auth client

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './login-style.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-glbca-gb.us.auth0.com"
+    clientId="fj2scFp0Wee7QMd4duzFAnznSAWspQhy"
+    // redirectUri={window.location.origin}
+    redirectUri={"http://localhost:3000"}  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+    
+);
