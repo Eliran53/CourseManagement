e01f9c8801b1cc04daff80e9bebe7ade2526ae47
eliran <elirantarka7@gmail.com>
2020-10-08 00:44:58 +0300

    components for contact us

---
+++ client/src/components/pages/contactus.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Header from '../Header';
+import ContactForm from '../ContactForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './contactus.css';
+
+class contactuss extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="col-md-6 offset-md-3">
+          <Header />
+          <ContactForm />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default contactuss;
