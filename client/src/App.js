17519a63fe2402fb2f1c9b14b982fd93623e659f
mulu <bani0123456@gmail.com>
2020-10-08 21:26:19 +0300

    v

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,6 @@ import Health from "./components/pages/Health";
 import PersonalDevelopment from "./components/pages/Personal-Development";
 import test from "./components/pages/test";
 import "./login-style.scss";
-import contact from "./components/pages/Contact";
-import Home from "./components/pages/Home";
 import loginButton from './components/loginbtn'
 import logoutButton from './components/logoutbtn'
 import contactus from './components/pages/contactus'
@@ -23,7 +21,6 @@ function App() {
         <Route path="/Makeup" component={Makeup} />
         <Route path="/Health" component={Health} />
         <Route path="/Personal-Development" component={PersonalDevelopment} />
-        <Route path="/Contact" component={contact} />
         <Route path="/loginout" component={loginButton} />
         <Route path="/Login-Register" component={logoutButton} />
         <Route path="/contactus" component={contactus} />
