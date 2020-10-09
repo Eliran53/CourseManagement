a2a2847466fc461671f16b2475cf54dd8d20a1f7
mulu <bani0123456@gmail.com>
2020-10-09 11:36:37 +0300

    v

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import Navbar from "./components/Navbar";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -11,6 +10,7 @@ import "./login-style.scss";
 import loginButton from './components/loginbtn'
 import logoutButton from './components/logoutbtn'
 import contactus from './components/pages/contactus'
+import LoginRegister from "./LoginAndRegister";
 
 
 function App() {
@@ -21,6 +21,7 @@ function App() {
         <Route path ="/Login-Register" component = {test}/>
         <Route path="/Makeup" component={Makeup} />
         <Route path="/Health" component={Health} />
+        <Route path="/Login" component={LoginRegister} />
         <Route path="/Personal-Development" component={PersonalDevelopment} />
         <Route path="/loginout" component={loginButton} />
         <Route path="/Login-Register" component={logoutButton} />
@@ -29,38 +30,6 @@ function App() {
        
       </Switch>
     </Router>
-  );
-=======
-import reactDOM from "react-dom";
-// import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
-// import Login from "./components/Login";
-// import Register from "./components/Register";
-import LoginRegister from "./LoginAndRegister";
-
-
-export default class App extends React.Component {
-  render() {
-    return (
-      // <BrowserRouter>
-      //   <div className="App">
-      //     <Link to='/login'> Login</Link>
-      //     <Link to='/register'>Register</Link>
-
-
-      //     <Switch>
-      //       <Route exact path='/login' component={Login} />
-      //       <Route exact path='/register' render={() => <Register />} />
-      //     </Switch>
-
-      //   </div>
-      // </BrowserRouter>
-
-      <div>
-        <LoginRegister/>
-      </div>
-    )
-  }
-
->>>>>>> origin/Einav-the-Queen
+  )
 }
 export default App;
