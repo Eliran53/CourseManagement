e87e5b1bb1afef4aefe72046619ae15438d41c6b
mulu <bani0123456@gmail.com>
2020-10-11 18:06:59 +0300

    v

--- modules/lecture-module.js
+++ modules/lecture-module.js
@@ -1,52 +1,35 @@
 const mongoose = require("mongoose");
-//  const schema = mongoose.Schema;
-// const lecture = new schema(
-//   {
-//     instructorID:{type:String,required:true},
-//     lectureName: { type: String, required: true },
-//     subjectID: { type: String, required: true, unique: true },
-//     duration: { type: String, required: true },
-//     maxCapacityParticipants: { type: Number, required: true },
-//     lectureDate: { type: Date, required: true },
-//     summery: { type: String, required: true },
-//     price: { type: Number, required: true },
-//     language: { type: String, required: true },
-//     rank: { type: String },
-//     recommended: { type: String, required: true },
-//     videos: { type: String, required: true },
-//     customerID: { type: String },
-//   },
-//   { timestamps: true }
-// );
-// module.exports = mongoose.model("lecture", lecture);
 
-const schema = mongoose.Schema
-const ObjectId = require('mongoose').Types.ObjectId;
+const schema = mongoose.Schema;
+const ObjectId = require("mongoose").Types.ObjectId;
 const lecture = new schema(
-    {
-    instructorID:{type:ObjectId,required:true},
-    lecture_name:{type:String},
-    subjectID :{type:ObjectId,required:true},
-    duration:{type:String,required:true},
-    maxCapacityParticipants:{type:Number,default:"Unlimited",required:true},
-    uploadDate:{type:Date,default:Date.now()},
-    lectureDate:{type:Date,required:true},
-    summery:{type:String,required:true},
-    price:{type:Number, required:true},
-    language:{type:String, required:true},
-    rank:{type:String},
-    recommended:{type:String},
-    videos:{type:String, required:true},
-    customersID:[{type:ObjectId}]
+  {
+    instructorID: { type: ObjectId, required: true },
+    lecture_name: { type: String },
+    subjectID: { type: ObjectId, required: true },
+    duration: { type: String, required: true },
+    maxCapacityParticipants: {
+      type: Number,
+      default: "Unlimited",
+      required: true,
     },
-    {timestamps:true},
-    
-)
-lecture.virtual('customers', {
-    ref: 'customer', 
-    localField: '_id', 
-    foreignField: 'lecture_id', 
- 
+    // uploadDate:{type:Date,default:Date.now()},
+    lectureDate: { type: Date, required: true },
+    summery: { type: String, required: true },
+    price: { type: Number, required: true },
+    language: { type: String, required: true },
+    rank: { type: String },
+    recommended: { type: String },
+    videos: { type: String, required: true },
+    customersID: [{ type: ObjectId }],
+  },
+  {
+    timestamps: true,
+  }
+);
+lecture.virtual("customers", {
+  ref: "customer",
+  localField: "_id",
+  foreignField: "lecture_id",
 });
-module.exports = mongoose.model("lecture", lecture)
-
+module.exports = mongoose.model("lecture", lecture);
