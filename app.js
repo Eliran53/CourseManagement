7ecce094b2934ec761b36c6ce35d88c1c0ab5171
revitalmatoko <revitalmatoko@gmail.com>
2020-10-12 17:40:42 +0300

    I fixed the app.js file

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ db.on("error", console.error.bind(console, "mongoDB connection error"));
 var indexRouter = require("./routes/index");
 //var usersRouter = require('./routes/users');
 var instructorsRouter = require("./routes/instructor-route");
-
 var contactusRouter = require("./routes/contactus-route");
 var authRouter = require("./routes/auth");
 var userRouter = require("./routes/user");
@@ -45,60 +44,17 @@ app.use(express.static(path.join(__dirname, "public")));
 // Added to serve client static files
 app.use(express.static(path.resolve(__dirname, "client/build")));
 
-app.use("/", indexRouter);
+// app.use('/', indexRouter);
+app.use('/api/users', userRouter);
+app.use('/api/instructors', instructorsRouter);
+app.use('/api/roles', rolesRouter);
+app.use('/api/subjects', subjectsRouter);
+app.use('/api/customers', customersRouter);
+app.use('/api/lectures', lectureRouter);
+app.use('/api/queries', queriesRouter);
+app.use('/api/auth',authRouter);
+app.use('/contactus',contactusRouter);
 
-// app.use('/users', usersRouter);
-// app.use('/instructors',instructorsRouter);
-// app.use('/roles',rolesRouter);
-// app.use('/subjects',subjectsRouter);
-// app.use('/contactus',contactusRouter);
-
-app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Headers",
-    "x-access-token, Origin, Content-Type, Accept"
-  );
-  next();
-});
-app.use("/",authRouter);
-app.use("/",userRouter);
-// app.use('/api/customers',customersRouter);
-// app.use('/api/lectures',lectureRouter)
-// require('./app/routes/auth.routes')(app);
-// require('./app/routes/user.routes')(app);
-app.post("/send", (req, res) => {
-  try {
-    const mailOptions = {
-      from: req.body.email,
-      to: "lecture867@gmail.com",
-      subject: req.body.subject,
-      html: req.body.message,
-    };
-
-    transporter.sendMail(mailOptions, (err, info) => {
-      res.send({
-        success: true,
-        message: "thank u for contacting us",
-      });
-    });
-  } catch (error) {
-    res.status(500).send(
-      {
-        success: false,
-        message: "something went wrong, please try later",
-      },
-      console.log("blaa1")
-    );
-  }
-});
-
-app.use("/api/users", userRouter);
-app.use("/api/instructors", instructorsRouter);
-app.use("/api/roles", rolesRouter);
-app.use("/api/subjects", subjectsRouter);
-app.use("/api/customers", customersRouter);
-app.use("/api/lectures", lectureRouter);
-app.use("/api/queries", queriesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
