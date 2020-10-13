bdfa91351c4cc06650a18a1c98c0883a5525e884
revitalmatoko <revitalmatoko@gmail.com>
2020-10-14 00:43:38 +0300

    I arranged the code on the client side

--- client/src/components/contact/contactus.js
+++ client/src/components/contact/contactus.js
@@ -1,24 +1,10 @@
-import React from 'react';
-import Header from './Header';
-import ContactForm from './ContactForm';
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import './contactus.css';
 
-function contactus() {
 
-    return (
-      <div >
-        <div>
-<<<<<<< HEAD
-          <Header />
-=======
-          {/* <Header /> */}
->>>>>>> origin/site-design
-          <ContactForm />
-        </div>
-      </div>
-    );
-  
-}
 
-export default contactus;
+
+
+
+
+
+
+          
---
+++ client/src/components/homePage/Video.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import '../css/video';
+import VideoTitel from '../homePage/VideoTitel';
+
+function Video() {
+
+    return (
+      <>
+        <div className="video">
+        <section className="page-section" id="services">
+        <div className="container">
+        <h2 className="text-center mt-0">Lectures of your choice</h2>
+        <div className="videofile"><VideoTitel/></div></div> </section> </div>
+       
+      </>
+    );
+  }
+  
+  export default Video;
+
