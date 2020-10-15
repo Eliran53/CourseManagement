207014218d30ad6927c2731792ed197cdbe3648e
mulu <bani0123456@gmail.com>
2020-10-15 20:07:43 +0300

    V

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,49 +1,28 @@
 import React from "react";
 import "../../src/App.css";
-import Navbar from '../components/Navbar1'
-import Footer from '../components/pages/general/Footer';
+import Navbar from "../components/Navbar";
+import Footer from "../components/pages/general/Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from '../components/Home';
-// import Navbar from '../components/pages/general/Navigation';
-// import Header from '../homePage/Header';
-// import About from '../homePage/About'
-// import Video from '../homePage/Video';
-// import ContactUs from '../contact/ContactUs';
-// import LoginRegister from '../LoginAndRegister';
-// import Categories from '../homePage/Categories';
+import Home from "../components/Home";
+import ContactUs from "../contact/ContactUs";
+import Login  from '../Login/index'
+import Signup  from '../Register/index'
+import Preview from '../homePage/Video'
 
 export default function App() {
   return (
-
-  <div className= "App">
-     <Router>
-      <Navbar />
-      <Switch>
-     <Route path = "/" exact component = {Home}/>
-         {/* <Route path="/Login-Register" component={LoginRegister} />
-        <Route path="/Categories" component={Categories} />
-         <Route path="/Header" component={Header} />
-         <Route path="/About" component={About} />
-       <Route path="/loginout" component={logoutButton} />
-        <Route exact path="/" component={Home} /> 
-       <Route exact path="/Preview" component={Video} />
-       <Route exact path="/Contact" component={ContactUs} />
-      */}
-      </Switch>
-      <Footer />
-    </Router>
-{/*  
-   <Navbar />   
-    <Header/>
-    <About/>
-    <Video/>
-   <ContactUs/> 
-    <Footer />*/}
-   
-
-
-  </div>
-  )
-} 
-
- 
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/contactUs" exact component={ContactUs} />
+          <Route path="/Login" exact component={Login} />
+          <Route path="/Signup" exact component={Signup} />
+          <Route path="/Preview" exact component={Preview} />
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,48 +1,31 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import '../components/css/navbar1.css';
-import Categories from './Categories';
-import Preview from './CustomLink';
-import ContactButton from './ContactButton';
-import Search from './Search';
-import About from './About';
-import ContactUs from '../contact/ContactUs';
-import { Link } from "react-router-dom";
-import Video from '../homePage/Video'
+import React from "react";
+import "../components/css/navbar1.css";
+import {Link} from 'react-router-dom'
 
-export default class Navbar extends Component {
-  links = [
-    {path : 'Categories', selected : false },
-    {path : 'Preview', selected : false },
-    {path : 'About', selected : false },
-    {path : 'Contact', selected : false },
-  
-     
-  ];
-  constructor(props){
-    super(props);
-    this.state = {
-      links: this.links
-    }
-
-  }
+class Links extends React.Component {
   render() {
     return (
-        <div className="Navbar">
-            <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
                   <div className="container">
-                  <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                  <div className="collapse navbar-collapse" id="navbarResponsive">
+                 
+                <div className="navbar-brand js-scroll-trigger" href="#page-top" >
+                       </div>
+                     <div className="md-form mt-0">
+                            <input className="form-control" type="text" placeholder="Search" aria-label="Search"/>
+                          </div> 
+               <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+               <div className="collapse navbar-collapse" id="navbarResponsive">
                     <ul className="navbar-nav ml-auto my-2 my-lg-0">
-
-                   
+                        <li className="nav-item"><Link to="/" className="nav-link js-scroll-trigger">Catgoris</Link></li>
+                        <li className="nav-item"><Link to="/Preview" className="nav-link js-scroll-trigger">preview</Link></li>
+                        <li className="nav-item"> <Link to="/contactUs" className="nav-link js-scroll-trigger">Contact</Link></li>
+                        <li className="nav-item"> <Link to="/Login" className="nav-link js-scroll-trigger">Login</Link></li>
+                        <li className="nav-item"> <Link to="/Signup" className="nav-link js-scroll-trigger">Sign up</Link></li>
                     </ul>
-                    </div>
-
-                  </div>
-            </nav>
-        
-        </div>
+                </div>
+            </div>
+        </nav>
     );
   }
 }
+export default Links;
