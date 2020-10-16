ec4287ef9b919f3eedc321f729c1a240cf13cd85
mulu <bani0123456@gmail.com>
2020-10-16 12:06:40 +0300

    V

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,5 +1,5 @@
 export const urlBase = () => {
-  const domain = process.env.REACT_APP_DOMAIN;
+  const domain = process.env.NODE_ENV === "production" ? process.env.REACT_APP_DOMAIN : "localhost:3000";
   const http = process.env.NODE_ENV === "production" ? "https" : "http";
 
   const url = `${http}://${domain}`;
