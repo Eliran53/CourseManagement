d5ff74ba014dc4c9a40967d031c91359171c94b5
einav biru <brwynb112@gmail.com>
2020-10-16 12:39:44 +0300

    Register/Login

--- client/src/LoginBox.jsx
+++ client/src/LoginBox.jsx
@@ -3,78 +3,77 @@ import axios from 'axios';
 
 class LoginBox extends React.Component {
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        email: "",
-        password: "",
-      };
-  
-    }
-  
-    osubmitLogin(e) {
-      console.log("Hello world!");
-      axios
-        .post('api/auth/signin', {
-          email: this.state.email,
-          password: this.state.password,
-  
-        })
-        .then(res => {
-          console.log('singin!!!');
-          this.props.onLogin();
-  
-  
-        }).catch(() => {
-          this.props.onError();
-          console.log('some ERROR!!!!!!!')
-        })
-    }
-  
-  
-  
-    render() {
-      return (
-        <div className="inner-container">
-          <div className="header">
-            Login
-          </div>
-          <div className="box">
-  
-            <div className="input-group">
-              <label htmlFor="Email">Email</label>
-              <input
-                type="text"
-                name="Email"
-                className="login-input"
-                placeholder="Email"
-                onChange={(e) => { this.setState({ email: e.target.value }) }} />
-            </div>
-  
-            <div className="input-group">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                name="password"
-                className="login-input"
-                placeholder="Password"
-                onChange={(e) => { this.setState({ password: e.target.value }) }} />
-            </div>
-  
-  
-  
-            <button
-              type="button"
-              className="login-btn"
-              onClick={this
-                .osubmitLogin
-                .bind(this)}>Login</button>
-  
-          </div>
-        </div>
-      );
-    }
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+
   }
-  export default LoginBox;
-  
+
+  osubmitLogin(e) {
+    console.log("Hello world!");
+    axios
+      .post('api/auth/signin', {
+        email: this.state.email,
+        password: this.state.password,
+
+      })
+      .then(res => {
+        console.log('singin!!!');
+        this.props.onLogin();
+
+
+      }).catch(() => {
+        this.props.onError();
+        console.log('some ERROR!!!!!!!')
+      })
+  }
+
+
+
+  render() { 
+    return (
+      <div className="inner-container">
+        <div className="header">
+          Login
+          </div>
+        <div className="box">
+
+          <div className="input-group">
+            <label htmlFor="Email">Email</label>
+            <input
+              type="text"
+              name="Email"
+              className="login-input"
+              placeholder="Email"
+              onChange={(e) => { this.setState({ email: e.target.value }) }} />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="login-input"
+              placeholder="Password"
+              onChange={(e) => { this.setState({ password: e.target.value }) }} />
+          </div>
+
+
+
+          <button
+            type="button"
+            className="login-btn"
+            onClick={this
+              .osubmitLogin
+              .bind(this)}>Login</button>
+
+        </div>
+      </div>
+    );
+  }
+
+}
+export default LoginBox;
