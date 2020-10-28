bd47d1e024f3c8778d1d74a9aabca5a6e8d36ef3
eliran <elirantarka7@gmail.com>
2020-10-28 03:33:40 +0200

    credit card

---
+++ client/src/components/credit-card/Credit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Credit.scss';
+import MainScreen from '../credit-card/MainScreen';
+
+function Credit() {
+    
+    return <MainScreen />;
+    
+}
+
+export default Credit;
---
+++ client/src/components/credit-card/MainScreen.js
@@ -0,0 +1,103 @@
+import React, { useState, useRef, useCallback } from "react";
+import CForm from "./form";
+import Card from "./card";
+import Button from "@material-ui/core/Button";
+import "./Credit.scss";
+import history from "../../history";
+
+const initialState = {
+  cardNumber: "#### #### #### ####",
+  cardHolder: "FULL NAME",
+  cardMonth: "",
+  cardYear: "",
+  cardCvv: "",
+  isCardFlipped: false,
+};
+
+const MainScreen = () => {
+  const [state, setState] = useState(initialState);
+  const [currentFocusedElm, setCurrentFocusedElm] = useState(null);
+
+  const updateStateValues = useCallback(
+    (keyName, value) => {
+      setState({
+        ...state,
+        [keyName]: value || initialState[keyName],
+      });
+    },
+    [state]
+  );
+
+  // References for the Form Inputs used to focus corresponding inputs.
+  let formFieldsRefObj = {
+    cardNumber: useRef(),
+    cardHolder: useRef(),
+    cardDate: useRef(),
+    cardCvv: useRef(),
+  };
+
+  let focusFormFieldByKey = useCallback((key) => {
+    formFieldsRefObj[key].current.focus();
+  });
+
+  // This are the references for the Card DIV elements.
+  let cardElementsRef = {
+    cardNumber: useRef(),
+    cardHolder: useRef(),
+    cardDate: useRef(),
+  };
+
+  let onCardFormInputFocus = (_event, inputName) => {
+    const refByName = cardElementsRef[inputName];
+    setCurrentFocusedElm(refByName);
+  };
+
+  const onPay = () => {
+    alert("thank u for buying");
+    history.push("/");
+    window.location.reload()
+  };
+
+  let onCardInputBlur = useCallback(() => {
+    setCurrentFocusedElm(null);
+  }, []);
+
+  return (
+    <div className="videofile">
+      <div className="wrapper">
+        <CForm
+          cardMonth={state.cardMonth}
+          cardYear={state.cardYear}
+          onUpdateState={updateStateValues}
+          cardNumberRef={formFieldsRefObj.cardNumber}
+          cardHolderRef={formFieldsRefObj.cardHolder}
+          cardDateRef={formFieldsRefObj.cardDate}
+          onCardInputFocus={onCardFormInputFocus}
+          onCardInputBlur={onCardInputBlur}
+        >
+          <Card
+            cardNumber={state.cardNumber}
+            cardHolder={state.cardHolder}
+            cardMonth={state.cardMonth}
+            cardYear={state.cardYear}
+            cardCvv={state.cardCvv}
+            isCardFlipped={state.isCardFlipped}
+            currentFocusedElm={currentFocusedElm}
+            onCardElementClick={focusFormFieldByKey}
+            cardNumberRef={cardElementsRef.cardNumber}
+            cardHolderRef={cardElementsRef.cardHolder}
+            cardDateRef={cardElementsRef.cardDate}
+          ></Card>
+        </CForm>
+        <div></div>
+      </div>
+      <div className="lll">
+        <Button variant="contained" color="secondary" onClick={onPay}>
+          Submit
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default MainScreen;
