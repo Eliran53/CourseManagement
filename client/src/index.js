b6870b3cd144289396e9d37d760a94cb86467392
eliran <elirantarka7@gmail.com>
2020-10-28 03:30:45 +0200

    /

--- client/src/index.js
+++ client/src/index.js
@@ -4,4 +4,4 @@ import App from "../src/app/App";
 import "./components/css/googleapis.css";
 import "./components/css/style.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render( <App />, document.getElementById("root"));
